bd8f935a70f1e8cd563abad406d6723851ccb737
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-02-17 16:08:52 +0100

    Using MutableList for now, LiveData when synchro with Database

--- app/src/main/java/fr/dleurs/android/contactapp/repository/ContactRepository.kt
+++ app/src/main/java/fr/dleurs/android/contactapp/repository/ContactRepository.kt
@@ -1,13 +1,26 @@
 package fr.dleurs.android.contactapp.repository
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import fr.dleurs.android.contactapp.model.Contact
 import fr.dleurs.android.contactapp.model.ContactRetrofitApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 class ContactRepository {
 
-    val contacts: LiveData<List<Contact>> = Transformations.map(ContactRetrofitApi.retrofitService.getContacts()) {
-        it.asDomainModel()
+    //val contacts: LiveData<List<Contact>>;
+    //val contacts: MutableLiveData<List<Contact>> = MutableLiveData<List<Contact>>()
+    val contacts: MutableList<Contact> = mutableListOf<Contact>()
+
+    suspend fun refreshContacts() {
+        withContext(Dispatchers.IO) {
+            Timber.d("refresh contacts is called");
+            val contactsNetwork = ContactRetrofitApi.retrofitService.getContacts()
+            contacts.addAll(contactsNetwork)
+        }
     }
+
 }
