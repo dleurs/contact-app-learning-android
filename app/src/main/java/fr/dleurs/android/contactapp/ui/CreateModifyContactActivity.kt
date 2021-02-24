06593d279bc859ba26d2088fd8a405e1200ffff4
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-02-24 16:29:53 +0100

    Better UI and start working on Intent new Contact

--- app/src/main/java/fr/dleurs/android/contactapp/ui/CreateModifyContactActivity.kt
+++ app/src/main/java/fr/dleurs/android/contactapp/ui/CreateModifyContactActivity.kt
@@ -1,8 +1,12 @@
 package fr.dleurs.android.contactapp.ui
 
+import android.app.Activity
+import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -18,7 +22,21 @@ class CreateModifyContactActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         setContentView(R.layout.create_modify_contact_activity)
 
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            //if (TextUtils.isEmpty(firs.text)) {
+            //   setResult(Activity.RESULT_CANCELED, replyIntent)
+            //} else {
+            //val word = editWordView.text.toString()
+            //replyIntent.putExtra(EXTRA_REPLY, word)
+            setResult(Activity.RESULT_OK, replyIntent)
+            //}
+            finish()
+        }
     }
 }
