fb832c2ed233687b99b0105efdf0530529aa5cb8
Dimitri Leurs <dimitri.leurs@numberly.com>
2021-02-26 14:45:42 +0100

    Details screen working, need to improve UI

--- app/src/main/java/fr/dleurs/android/contactapp/ui/detailsContact/DetailsContactActivity.kt
+++ app/src/main/java/fr/dleurs/android/contactapp/ui/detailsContact/DetailsContactActivity.kt
@@ -2,27 +2,32 @@ package fr.dleurs.android.contactapp.ui.detailsContact
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import fr.dleurs.android.contactapp.R
 import fr.dleurs.android.contactapp.database.ContactDatabase
+import fr.dleurs.android.contactapp.databinding.DetailsContactActivityBinding
 import fr.dleurs.android.contactapp.model.Contact
 import timber.log.Timber
 
 class DetailsContactActivity : AppCompatActivity() {
 
+    private lateinit var binding: DetailsContactActivityBinding
+    private lateinit var contactPassed: Contact
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val intentData = intent.getParcelableExtra<Contact>("contact")?.let { reply ->
-            val contact = ContactDatabase(
+            contactPassed = Contact(
+                id=reply.id,
                 firstName = reply.firstName,
                 lastName = reply.lastName,
                 mail = reply.mail
             )
-            Timber.i("Intent received + " + contact.toString())
+            Timber.i("Intent received + " + contactPassed.toString())
         }
-
-        setContentView(R.layout.details_contact_activity)
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.details_contact_activity)
+        binding.apply { contact = contactPassed }
     }
 }
